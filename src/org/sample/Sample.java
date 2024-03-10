a1564c3aef942eb1cce920a3624b8726906545ce
velsha123 <vel123sha@gmail.com>
2024-03-10 19:00:05 +0530

    added few lines

---
+++ src/org/sample/Sample.java
@@ -0,0 +1,15 @@
+package org.sample;
+
+public class Sample {
+
+	private void add() {
+	System.out.println("sample");
+
+	}
+	
+	public static void main(String[] args) {
+		Sample s = new Sample();
+		s.add();
+	}
+	
+}
